d43e5952a55a10c10dc7f61a7e49f699f72a536f
GURPAHUL-SINGH <gurpahulsingh13@gmail.com>
2018-05-18 09:49:11 -0500

    changes addded in big picture

--- catisdone/cat.js
+++ catisdone/cat.js
@@ -1,18 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     const cat1 = document.querySelector('#catimg1')
+    const cat2 = document.querySelector('#catimg2')
+    const cat3 = document.querySelector('#catimg3')
+    const cat4 = document.querySelector('#catimg4')
+
+    cat = [cat1, cat2, cat3, cat4]
+    counts = [0,0,0,0]
+    for (cat of cats){
+     cat.addEventListener('click' ,function(){
+       for (count of counts){
+           count.innerHTML++
+       }
+     })
+    }
+
     const count1 = document.querySelector('#click1')
     const count2 = document.querySelector('#click2')
 
     const catname2 = document.querySelector('.catname2')
-    const cat2 = document.querySelector('#catimg2')
     const catname1 = document.querySelector('.catname1')
+    
     cat1.addEventListener('click', function () {
         count1.innerHTML++
     })
+    
     cat2.addEventListener('click', function () {
         count2.innerHTML++
     })
+    
     catname1.textContent = "CAT1";
     catname2.textContent = "CAT2";
 
